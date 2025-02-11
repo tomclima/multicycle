9618a5a522b2435d02926e7b2d4089e5a5e273f5
tcl <tcl@cin.ufpe.br>
2025-02-11 10:10:09 -0300

    sign ext

---
+++ parts-made/sign_ext.v
@@ -0,0 +1,8 @@
+module sign_ext_16b(
+    input       wire        [15:0]      data_0,
+    output      wire        [31:0]      data_out
+);
+
+    assign data_out = data_0[15] ? {{16'b1}, data_0} : {{16'b0}, data_0};
+
+endmodule
