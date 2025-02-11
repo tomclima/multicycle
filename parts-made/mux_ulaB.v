896822ce257e3d8ca43bd21fb8efaca3f7590520
tome <tomecostalima1228@gmail.com>
2025-02-11 16:48:41 -0300

    alusrcb + sign extend

--- parts-made/mux_ulaB.v
+++ parts-made/mux_ulaB.v
@@ -1,11 +1,14 @@
-module mux_aluB(
-    input wire      [3:0]       selector;
-    input wire      [31:0]      data_0;
-    input wire      [31:0]      data_2;
-    input wire      [31:0]      data_3;
-    output wire     [31:0]      data_out;
+module mux_pcsource(
+    input wire      [3:0]       selector,
+    input wire      [31:0]      data_0,
+    input wire      [31:0]      data_2,
+    input wire      [31:0]      data_3,
+    output wire     [31:0]      data_out
 );
 
-assign data_out = (selector == 0)*(data_0) + (selector == 1)*(4) + (selector == 2)*(data_2) + (selector == 3)*(data+3);
+assign data_out = (selector == 4'b0000) ? data_0 : 
+                  (selector == 4'b0001) ? 4 : 
+                  (selector == 4'b0010) ? data_2 : 
+                  (selector == 4'b0011) ? data_3 : 32'b0;  // Default case
 
 endmodule
--- parts-made/sign_ext.v
+++ parts-made/sign_ext.v
@@ -5,4 +5,4 @@ module sign_ext_16b(
 
     assign data_out = data_0[15] ? {{16'b1}, data_0} : {{16'b0}, data_0};
 
-endmodule
+endmodule 
