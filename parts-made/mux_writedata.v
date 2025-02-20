0ca2b770b266002876e5f511e80417b85e740828
tcl <tomecostalima1228@gmail.com>
2025-02-20 16:46:27 -0300

    name correction

--- parts-made/mux_writedata.v
+++ parts-made/mux_writedata.v
@@ -1,15 +1,4 @@
 module mux_writedata(
-    input   wire            MemToReg,
-    input   wire    [31:0]  write_src_mux_out,
-    input   wire    [31:0]  byte_mux_out,
-    output  wire    [31:0]  data_out
-);
-
-    assign data_out = selector ? data_0, data_1;
-
-endmodule;
-
-module mux_aluB(
     input wire      [3:0]       selector,
     input wire      [31:0]      write_src_mux_out,
     input wire      [31:0]      mem_out,
