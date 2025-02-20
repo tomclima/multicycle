13aa0b19a8f2218f285cf0ec0d567143834d0aad
tcl <tomecostalima1228@gmail.com>
2025-02-20 16:47:07 -0300

    logic for jal

--- parts-made/mux_writereg.v
+++ parts-made/mux_writereg.v
@@ -1,4 +1,4 @@
-module mux_aluB(
+module mux_writereg(
     input wire      [3:0]       selector,
     input wire      [4:0]      data_0,
     input wire      [15:0]      data_1,
@@ -7,7 +7,7 @@ module mux_aluB(
 
 assign data_out = (selector == 4'b0000) ? data_0 : 
                   (selector == 4'b0001) ? data_1 : 
-                  (selector == 4'b0010) ? 32'd29 : 
-                  (selector == 4'b0011) ? 32'd31 : 32'b0;  // Default case
+                  (selector == 4'b0010) ? 32'd31 : 
+                  (selector == 4'b0011) ? 32'd29 : 32'b0;  // Default case
 
 endmodule
