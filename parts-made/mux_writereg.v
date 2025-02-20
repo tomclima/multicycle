4e55ef888d1ccdf7cd74e10f5d883a897b6abfda
tcl <tomecostalima1228@gmail.com>
2025-02-20 16:47:00 -0300

    added logic for jal

--- parts-made/mux_writereg.v
+++ parts-made/mux_writereg.v
@@ -7,6 +7,7 @@ module mux_aluB(
 
 assign data_out = (selector == 4'b0000) ? data_0 : 
                   (selector == 4'b0001) ? data_1 : 
-                  (selector == 4'b0010) ? 32'd29 : 32'b0;  // Default case
+                  (selector == 4'b0010) ? 32'd29 : 
+                  (selector == 4'b0011) ? 32'd31 : 32'b0;  // Default case
 
 endmodule
