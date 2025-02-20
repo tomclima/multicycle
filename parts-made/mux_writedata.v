130ad85470cc8d32926f92fd0f0d45365706125e
tcl <tomecostalima1228@gmail.com>
2025-02-20 16:30:53 -0300

    incorporated changes to writedata

--- parts-made/mux_writedata.v
+++ parts-made/mux_writedata.v
@@ -8,3 +8,16 @@ module mux_writedata(
     assign data_out = selector ? data_0, data_1;
 
 endmodule;
+
+module mux_aluB(
+    input wire      [3:0]       selector,
+    input wire      [31:0]      write_src_mux_out,
+    input wire      [31:0]      mem_out,
+    output wire     [31:0]      data_out
+);
+
+assign data_out = (selector == 4'b0000) ? write_src_mux_out : 
+                  (selector == 4'b0001) ? mem_out: 
+                  (selector == 4'b0010) ? 32'd227 : 32'b0;  // Default case
+
+endmodule
