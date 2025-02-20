59c77ed5d7da21319622385a7f368155e3fd86fb
tcl <tomecostalima1228@gmail.com>
2025-02-20 16:26:36 -0300

    same as beforw

--- parts-made/mux_writereg.v
+++ parts-made/mux_writereg.v
@@ -1,10 +1,12 @@
-module mux_writereg(
-    input   wire                selector,
-    input   wire        [4:0]   data_0,
-    input   wire        [15:0]  data_1,
-    output  wire        [4:0]   data_out
+module mux_aluB(
+    input wire      [3:0]       selector,
+    input wire      [4:0]      data_0,
+    input wire      [15:0]      data_1,
+    output wire     [4:0]      data_out
 );
 
-    assign data_out = selector ? data_1[15:11] : data_0;
+assign data_out = (selector == 4'b0000) ? data_0 : 
+                  (selector == 4'b0001) ? data_1 : 
+                  (selector == 4'b0010) ? 32'd29 : 32'b0;  // Default case
 
 endmodule
